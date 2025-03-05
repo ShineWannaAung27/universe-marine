7e977c2fa6406de9c1706fcdcee06e8895271397
ShineWannaAung27 <shinewanaaung@hmmserver.net>
2025-03-05 12:25:05 +0700

    icon update

--- services.php
+++ services.php
@@ -51,9 +51,11 @@ include 'layout/layout.php';
             <a href="services-certificate.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #D9D9D9;">
                     <div class="text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
                             <path d="M128,136a8,8,0,0,1-8,8H72a8,8,0,0,1,0-16h48A8,8,0,0,1,128,136Zm-8-40H72a8,8,0,0,0,0,16h48a8,8,0,0,0,0-16Zm112,65.47V224A8,8,0,0,1,220,231l-24-13.74L172,231A8,8,0,0,1,160,224V200H40a16,16,0,0,1-16-16V56A16,16,0,0,1,40,40H216a16,16,0,0,1,16,16V86.53a51.88,51.88,0,0,1,0,74.94ZM160,184V161.47A52,52,0,0,1,216,76V56H40V184Zm56-12a51.88,51.88,0,0,1-40,0v38.22l16-9.16a8,8,0,0,1,7.94,0l16,9.16Zm16-48a36,36,0,1,0-36,36A36,36,0,0,0,232,124Z"></path>
-                        </svg>
+                        </svg> -->
+                        <img src="assets/images/services1.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -68,8 +70,10 @@ include 'layout/layout.php';
         <div class="col-md-4">
             <a href="services-deck.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #F2F2F2;">
-                    <div class="text-center" style="font-size: 53px;">
-                        <i class="fa-solid fa-ship"></i>
+                    <div class="text-center">
+                        <!-- <i class="fa-solid fa-ship"></i> -->
+                        <img src="assets/images/services2.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -84,9 +88,10 @@ include 'layout/layout.php';
             <a href="services-junior.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #D9D9D9;">
                     <div class="text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
                             <path d="M117.25,157.92a60,60,0,1,0-66.5,0A95.83,95.83,0,0,0,3.53,195.63a8,8,0,1,0,13.4,8.74,80,80,0,0,1,134.14,0,8,8,0,0,0,13.4-8.74A95.83,95.83,0,0,0,117.25,157.92ZM40,108a44,44,0,1,1,44,44A44.05,44.05,0,0,1,40,108Zm210.14,98.7a8,8,0,0,1-11.07-2.33A79.83,79.83,0,0,0,172,168a8,8,0,0,1,0-16,44,44,0,1,0-16.34-84.87,8,8,0,1,1-5.94-14.85,60,60,0,0,1,55.53,105.64,95.83,95.83,0,0,1,47.22,37.71A8,8,0,0,1,250.14,206.7Z"></path>
-                        </svg>
+                        </svg> -->
+                        <img src="assets/images/crew.png" alt="">
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -101,9 +106,11 @@ include 'layout/layout.php';
             <a href="services-gallery.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #F2F2F2;">
                     <div class="text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
                             <path d="M231.65,194.55,198.46,36.75a16,16,0,0,0-19-12.39L132.65,34.42a16.08,16.08,0,0,0-12.3,19l33.19,157.8A16,16,0,0,0,169.16,224a16.25,16.25,0,0,0,3.38-.36l46.81-10.06A16.09,16.09,0,0,0,231.65,194.55ZM136,50.15c0-.06,0-.09,0-.09l46.8-10,3.33,15.87L139.33,66Zm6.62,31.47,46.82-10.05,3.34,15.9L146,97.53Zm6.64,31.57,46.82-10.06,13.3,63.24-46.82,10.06ZM216,197.94l-46.8,10-3.33-15.87L212.67,182,216,197.85C216,197.91,216,197.94,216,197.94ZM104,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V48A16,16,0,0,0,104,32ZM56,48h48V64H56Zm0,32h48v96H56Zm48,128H56V192h48v16Z"></path>
-                        </svg>
+                        </svg> -->
+                        <img src="assets/images/services4.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -117,8 +124,10 @@ include 'layout/layout.php';
         <div class="col-md-4">
             <a href="services-passenger.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #D9D9D9;">
-                    <div class="text-center" style="font-size: 53px;">
-                        <i class="fa-solid fa-person-walking-luggage"></i>
+                    <div class="text-center">
+                        <!-- <i class="fa-solid fa-person-walking-luggage"></i> -->
+                        <img src="assets/images/services5.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -132,8 +141,10 @@ include 'layout/layout.php';
         <div class="col-md-4">
             <a href="services-seminar.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #F2F2F2;">
-                    <div class="text-center" style="font-size: 53px;transform: rotate(-45deg);">
-                        <i class="fa-solid fa-award"></i>
+                    <div class="text-center" ">
+                        
+                        <img src=" assets/images/services6.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
@@ -148,9 +159,11 @@ include 'layout/layout.php';
             <a href="services-skill.php" class="text-decoration-none">
                 <div class="card service-card" style="width: auto;background-color: #D9D9D9;">
                     <div class="text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#000000" viewBox="0 0 256 256">
                             <path d="M128,136a8,8,0,0,1-8,8H72a8,8,0,0,1,0-16h48A8,8,0,0,1,128,136Zm-8-40H72a8,8,0,0,0,0,16h48a8,8,0,0,0,0-16Zm112,65.47V224A8,8,0,0,1,220,231l-24-13.74L172,231A8,8,0,0,1,160,224V200H40a16,16,0,0,1-16-16V56A16,16,0,0,1,40,40H216a16,16,0,0,1,16,16V86.53a51.88,51.88,0,0,1,0,74.94ZM160,184V161.47A52,52,0,0,1,216,76V56H40V184Zm56-12a51.88,51.88,0,0,1-40,0v38.22l16-9.16a8,8,0,0,1,7.94,0l16,9.16Zm16-48a36,36,0,1,0-36,36A36,36,0,0,0,232,124Z"></path>
-                        </svg>
+                        </svg> -->
+                        <img src="assets/images/services1.png" alt="">
+
                     </div>
                     <div class="card-body pt-1">
                         <div class="text-center px-5">
